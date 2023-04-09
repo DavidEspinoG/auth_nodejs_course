6e88df8510d5ac6612b1b45197c9dfbf25d90830
David Espino <davidespinog@gmail.com>
2023-04-09 15:48:49 -0600

    Add change password endpoint

--- services/auth.service.js
+++ services/auth.service.js
@@ -30,7 +30,7 @@ class AuthService {
       token
     }
   }
-  async sendRecovery(){
+  async sendRecovery(email){
     const user = await service.findByEmail(email);
     if (!user) {
       throw boom.unauthorized();
@@ -39,7 +39,8 @@ class AuthService {
       sub: user.id,
     }
     const token = jwt.sign(payload, config.jwtSecret, {expiresIn: '15min'});
-    const link = `http://localhost/3000?token=${token}`
+    const link = `http://localhost/3000?token=${token}`;
+    await service.update(user.id, {recoveryToken: token});
     const info = {
       from: config.gmailUser,
       to: email,
@@ -51,10 +52,6 @@ class AuthService {
     return rta;
   }
   async sendMail(infoMail){
-    const user = await service.findByEmail(email);
-    if (!user) {
-      throw boom.unauthorized();
-    }
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
       port: 465,
@@ -69,6 +66,26 @@ class AuthService {
       message: 'Mail sent'
     }
   }
+  async changePassword (token, newPassword){
+    try {
+      const payload = jwt.verify(token, config.jwtSecret);
+      const user = await service.findOne(payload.sub);
+      if (user.recoveryToken !== token) {
+        throw boom.unauthorized();
+      }
+      const hash = await bcrypt.hash(newPassword, 10);
+      await service.update(user.id, {
+        recoveryToken: null, 
+        password: hash,
+      });
+      return {
+        password: "The password has been changed"
+      }
+    } catch (error) {
+      throw boom.unauthorized();
+    }
+  }
 }
 
 module.exports = AuthService;
+
